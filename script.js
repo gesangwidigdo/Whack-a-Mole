afcfd686deb60ecb61016336ab0b77719dbd7c2a
Gesang Widigdo <gesangwidigdo@gmail.com>
2023-10-22 21:28:14 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const dirt = document.querySelectorAll('.dirt');
+const mole = document.querySelectorAll('.mole');
+const scoreBoard = document.querySelector('.score-board');
+
+let dirtBefore;
+let done;
+let score;
+
+function randomDirt(dirt) {
+    const rd = Math.floor(Math.random() * dirt.length);
+    const randDirt = dirt[rd];
+
+    if (randDirt == dirtBefore) {
+        randomDirt(dirt);
+    }
+
+    dirtBefore = randDirt;
+    
+    return randDirt;
+}
+
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function showMole() {
+    const randDirt = randomDirt(dirt);
+    const randTime = randomTime(800, 1000);
+    randDirt.classList.add('show');
+
+    setTimeout(() => {
+        randDirt.classList.remove('show');
+        if (!done) {
+            showMole();
+        }
+    }, randTime);
+}
+
+function start() {
+    done = false;
+    score = 0;
+    scoreBoard.textContent = 0;
+    showMole();
+    setTimeout(() => {
+        done = true;
+    }, 10000);
+}
+
+function whack() {
+    score++;
+    this.parentNode.classList.remove('show');
+    scoreBoard.textContent = score;
+}
+
+mole.forEach(moles => {
+    moles.addEventListener('click', whack)
+});
